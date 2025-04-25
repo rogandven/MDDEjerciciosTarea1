22e01871e8d7de9e143b03d5ee5814a2c397a5bf
rogandven <roger.venegas2301@alumnos.ubiobio.cl>
2025-04-25 00:40:19 -0400

    Added exercises 7 and 8

---
+++ ejercicio7.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// Crea un array de números y utiliza filter para obtener los números mayores a 5.
+
+function printArrayWithName (name, array) {
+    if (typeof(name) != "string") {
+        throw new TypeError("name expected, " + typeof(name) + " given");
+    }
+    if (!(Array.isArray(array))) {
+        throw new TypeError("array expected, " + typeof(array) + " given");
+    }
+    name = name.trim();
+    process.stdout.write(name + ": ");
+    console.log(array);
+}
+
+const array1 = [1, 2, 4, 5, 10, 22, 420];
+const array2 = array1.filter((i) => {
+    if (i > 5) {
+        return i;
+    }
+});
+
+printArrayWithName("Arreglo 1", array1);
+printArrayWithName("Arreglo 2", array2);
---
+++ ejercicio8.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// Crea un array de números y usa reduce para obtener la suma total.
+
+function printVariableWithName (name, variable) {
+    if (typeof(name) != "string") {
+        throw new TypeError("name expected, " + typeof(name) + " given");
+    }
+
+    name = name.trim();
+    process.stdout.write(name + ": ");
+    console.log(variable);
+}
+
+const array1 = [100, 200, 300, 400, 500, 600, 700, 800, 900];
+const sum = array1.reduce((accumulator, current) => {
+    return accumulator + current;
+});
+
+printVariableWithName("Arreglo", array1);
+printVariableWithName("Suma", sum);
